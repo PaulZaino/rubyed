fdde14141ef725b5a45cb2e08f3782b86393e174
Paul Zaino <pjzaino@gmail.com>
2011-12-09 16:37:37 -0800

    added ex 1 2 and 3

---
+++ ex1.rb
@@ -0,0 +1,8 @@
+puts "Hellow World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rther you 'not'."
+puts 'I "said" do not touch this.'
+puts "One more line"
