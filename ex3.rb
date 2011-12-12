475b915457416f8f1e63500355fb50c09c61ad2e
Paul Zaino <pjzaino@gmail.com>
2011-12-12 13:47:10 -0800

    re-did exercise 3 and included comments

---
+++ ex3.rb
@@ -0,0 +1,35 @@
+# prints text in quotes
+puts "I will now count my chickens:"
+
+# prints Hens then does the operation 30 divided by 6 then adds 25 it then prints the result 30
+puts "Hens", 25 + 30 / 6
+# prints Roosters then does 25 times 3, then taskes the result mod 4 then subtracts the result from 100, thes prints the result 97
+puts "Roosters", 100 - 25 * 3 % 4
+
+# prints thext in quotes
+puts "Now I will count the eggs:"
+
+# does the operation 4 mod 2 and 1 divided by for, then subtracts the later from the former, it then adds 3 plus 2 plus 1 minus 4 plus the result plus 6, then it prints the result 7
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# prints the text in quotes including the numbers and math operators
+puts "Is it true that 3 + 2 <  5 - 7?"
+
+# does the operation 3 + 2 = 5 and 5 - 7 = -2, then it prints flase since the statement is false
+puts 3 + 2 < 5 - 7
+
+# prints all thats in the quotes then does the following math and prints the solution
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+# prints the text in quotes
+puts "Oh, that's why it's false."
+
+# prints the text in quotes
+puts "How about some more."
+
+# prints the text in quotes then does the operation to result as true or false
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2 
+
