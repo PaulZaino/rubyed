049d9e73b93b2e2c83107f906181d21d23abb1ce
Paul Zaino <pjzaino@gmail.com>
2011-12-13 13:19:42 -0800

    commiting from previous work

---
+++ ex3.rb
@@ -0,0 +1,35 @@
+# Prints the text in quotes
+puts "I will now count my chickens:"
+
+# Prints hens then does the following math
+puts "Hens", 25.0 + 30.0 / 6.0
+# Prints roosters then does the following math, mod gives remainder
+puts "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# Prints text in quotes
+puts "Now I will count the eggs:"
+
+# Does the math
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Prints the question in quotes
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Does the math then tells if the statement is true or false
+puts 3 + 2  < 5 - 7
+
+# Prints text in quotes and then does the following math
+puts "What is 3 + 2?", 3 + 2 
+puts "What is 5 - 7?", 5 - 7
+
+# Prints text in quotes
+puts "Oh, that's why it's false."
+
+# Prints text in quotes
+puts "How about some more."
+
+# Prints the text in quotes then tells if the statement is true or false
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it les or equal?", 5 <= -2
+
