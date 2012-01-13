f093b2ef2363dcc443587aa84e4d9db27dd4147d
Paul Zaino <pjzaino@gmail.com>
2012-01-12 16:29:33 -0800

    midified ex11.rb

--- ex11.rb
+++ ex11.rb
@@ -7,4 +7,8 @@ weight = gets.chomp()
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
 
-#Skipped excredit for now...
+#WTF does get and chomp do?
+
+#gets accepts a single line of data from the standard input - the keyboard in this case - and assigns the string typed by the user to city. The standard input is a default stream supplied by many operating systems that relates to the standard way to accept input from the user. In our case, the standard input is the keyboard.
+
+#chomp is a string method and gets retrieves only strings from your keyboard. You must have realised that gets returns a string and a '\n' character, while chomp removes this '\n'.
